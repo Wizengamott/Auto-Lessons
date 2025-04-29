fbdc83c738db9fafd586e0d63c1da8da3f8e27ba
Olexandr Butenko <butik.tks@gmail.com>
2025-04-29 13:21:54 +0300

    another commit

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-// app.js
+import { add, subtract } from './math.js';
 
-const mathModule = require('./math.js');
-
-const sum = mathModule.add(5, 3);
-const difference = mathModule.subtract(5, 3);
+const sum = add(5, 3);
+const difference = subtract(5, 3);
 
 console.log(sum); // Output: Sum: 8
 console.log(difference); // Output: Difference: 2
--- math.js
+++ math.js
@@ -1,6 +1,4 @@
 // math.js
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-
-module.exports = { add, subtract };
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
