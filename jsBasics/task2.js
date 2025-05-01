ab2781da38166f136e126025690c35eb3bc942e5
Olexandr Butenko <butik.tks@gmail.com>
2025-05-01 13:30:28 +0300

    lesson 4 tasks

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let personOne = "Olexandr";
+let personTwo = "Nataliia";
+
+let greetingOne = "Hello, " + personOne + ' and ' + personTwo + "!";
+console.log(greetingOne);
+
+let greetingTwo = `Hello, ${personOne} and ${personTwo}!`;
+console.log(greetingTwo);
