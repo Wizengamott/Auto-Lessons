0225396acf0067777a65165dd7fc7dfe780d7da8
Olexandr Butenko <butik.tks@gmail.com>
2025-05-05 21:18:48 +0300

    lesson_5

---
+++ lesson_5/task3.js
@@ -0,0 +1,20 @@
+// 3.1 FOR
+
+let number1 = 7;
+
+for (let i = 0; i <= 10; i++) {
+    let mutliplication = number1 * i;
+    console.log(number1 + " x " + i + " = " + mutliplication);
+}
+
+// 3.2 WHILE
+console.log("------------------------");
+
+let number2 = 7;
+let i = 0;
+
+while (i <= 10) {
+    let mutliplication = number2 * i;
+    console.log(number2 + " x " + i + " = " + mutliplication);
+    i++;
+}
